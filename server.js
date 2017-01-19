0b12b3c63435ab4936b72a6c6a650ff285bb5f65
SonMiguel <nodefeat2n@gmail.com>
2017-01-19 22:31:52 +0900

    update 기능 서버로직으로 옮김

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ var http = require('http'),
     server = http.Server(app);
 
 var playerSpeed = 200;
+var bulletSpeed = 500;
 var width = 512;
 var height = 768;
+var lastFire = Date.now();
+var bullets = [];
+var score = 0;
 
 /* Express server set up. */
 
@@ -60,9 +64,13 @@ function GameRoom(max_player_count) {
     this.player_host;
     this.host_pos = { x:0, y:0 };
     this.host_id;
+    this.host_sprite_size = { w:128, h:124 };
     this.player_client;
     this.client_pos = { x:0, y:0 };
     this.client_id;
+    this.client_sprite_size = { w:128, h:119 };
+    this.client_live = true;
+    this.client_dead_time = true;
     this.player_count = max_player_count;
     this.current_player_count = 0;
 };
@@ -123,8 +131,15 @@ sio.sockets.on('connection', function(client){
     client.on('move', function(dir, id){
         console.log('\t move packet ID : ' + id );
 
-        var msg = {dir:dir, id:id};
-        msg_array.push( msg );
+        var msg = {event: 'move', dir:dir, id:id};
+        msg_array.push(msg);
+    });
+
+    client.on('fire', function(id){
+        console.log('\t fire packet ID : ' + id);
+
+        var msg = {event: 'fire', id: id};
+        msg_array.push(msg);
     });
 }) //sio.sockets.on connection
 
@@ -152,47 +167,9 @@ var update = function(t) {
     var now = Date.now();
     var dt = (now - lastTime) / 1000.0;
 
-
-
-    msg_array.forEach(function(msg, index, object) {
-        // 임시
-        if(msg.id == g1.host_id)
-        {
-            switch(msg.dir)
-            {
-                case 'KEY_LEFT':
-                    g1.host_pos.x -= 1;
-                    break;
-                case 'KEY_RIGHT':
-                    g1.host_pos.x += 1;
-                    break;
-            }
-
-            // broadcast
-            g1.player_host.emit('move', g1.host_pos, msg.id);
-            g1.player_client.emit('move', g1.host_pos, msg.id);
-        }
-        else if(msg.id == g1.client_id)
-        {
-            switch(msg.dir)
-            {
-                case 'KEY_LEFT':
-                    g1.client_pos.x -= 1;
-                    break;
-                case 'KEY_RIGHT':
-                    g1.client_pos.x += 1;
-                    break;
-            }
-
-            // broadcast
-            g1.player_host.emit('move', g1.client_pos, msg.id);
-            g1.player_client.emit('move', g1.client_pos, msg.id);
-        }
-
-        object.splice(index, 1);
-    });
-
-
+    updateEntities(dt);
+    checkPlayerBounds();
+    checkCollisions();
 
     lastTime = now;
 
@@ -202,3 +179,181 @@ var update = function(t) {
 }; //game_core.update
 
 update( new Date().getTime() );
+
+function checkPlayerBounds() {
+    // Check bounds
+    if(g1.client_pos.x < 0) {
+        g1.client_pos.x = 0;
+    }
+    else if(g1.client_pos.x > width - g1.client_sprite_size.w) {
+        g1.client_pos.x = width - g1.client_sprite_size.w;
+    }
+
+    if(g1.host_pos.x < 0) {
+        g1.host_pos.x = 0;
+    }
+    else if(g1.host_pos.x > width - g1.host_sprite_size.w) {
+        g1.host_pos.x = width - g1.host_sprite_size.w;
+    }
+}
+
+function updateEntities(dt) {
+    // Update all the bullets
+    for(var i=0; i<bullets.length; i++) {
+        var bullet = bullets[i];
+
+        switch(bullet.dir) {
+        case 'up': bullet.pos[1] -= bulletSpeed * dt; break;
+        case 'down': bullet.pos[1] += bulletSpeed * dt; break;
+        default:
+            bullet.pos[0] += bulletSpeed * dt;
+        }
+
+        // broadcast
+        g1.player_host.emit('bullet move', bullet.pos, i);
+        g1.player_client.emit('bullet move', bullet.pos, i);
+
+        // Remove the bullet if it goes offscreen
+        if(bullet.pos[1] < 0 || bullet.pos[1] > height ||
+           bullet.pos[0] > width) {
+            bullets.splice(i, 1);
+            i--;
+
+            // broadcast
+            g1.player_host.emit('bullet remove', i);
+            g1.player_client.emit('bullet remove', i);
+        }
+    }
+
+    msg_array.forEach(function(msg, index, object) {
+        // 임시
+        if(msg.id == g1.host_id)
+        {
+            switch(msg.event)
+            {
+                case 'move':
+                    switch(msg.dir)
+                    {
+                        case 'KEY_LEFT':
+                            g1.host_pos.x -= 1;
+                            break;
+                        case 'KEY_RIGHT':
+                            g1.host_pos.x += 1;
+                            break;
+                    }
+
+                    // broadcast
+                    g1.player_host.emit('move', g1.host_pos, msg.id);
+                    g1.player_client.emit('move', g1.host_pos, msg.id);
+                    break;
+                    
+                case 'fire':
+                    if(Date.now() - lastFire > 500) {
+                        var x = g1.host_pos.x + g1.host_sprite_size.w / 2;
+                        var y = (g1.host_pos.y + g1.host_sprite_size.h / 2) + 56;
+
+                        console.log("똥 객체 생성 완료");
+                        console.log("똥 x : " + x);
+                        console.log("똥 y : " + y);
+                        console.log("똥 lastFire : " + lastFire);
+                        // 똥 객체 생성
+                        bullets.push({pos: [x, y], spriteSize: [48, 56], dir: 'down'});
+
+                        g1.player_host.emit('fire', x, y);
+                        g1.player_client.emit('fire', x, y);
+
+                        lastFire = Date.now();
+                    }
+                    break;
+            }
+        }
+        else if(msg.id == g1.client_id)
+        {
+            switch(msg.event)
+            {
+                case 'move':
+                    switch(msg.dir)
+                    {
+                        case 'KEY_LEFT':
+                            g1.client_pos.x -= 100 * dt;
+                            break;
+                        case 'KEY_RIGHT':
+                            g1.client_pos.x += 100 * dt;
+                            break;
+                    }
+
+                    // broadcast
+                    g1.player_host.emit('move', g1.client_pos, msg.id);
+                    g1.player_client.emit('move', g1.client_pos, msg.id);
+                    break;
+
+                case 'fire':
+                    // 한쪽은 싸는게 불가능하다
+                    break;
+            }
+        }
+
+        object.splice(index, 1);
+    });
+
+    if(false == g1.client_live) {
+		if( Date.now() - g1.client_dead_time > 5000 )
+		{
+			g1.client_live = true;
+
+            // broadcast
+            g1.player_host.emit('respawn');
+            g1.player_client.emit('respawn');
+		}
+	}
+}
+
+// Collisions
+
+function collides(x, y, r, b, x2, y2, r2, b2) {
+    return !(r <= x2 || x > r2 ||
+             b <= y2 || y > b2);
+}
+
+function boxCollides(pos, size, pos2, size2) {
+    return collides(pos[0], pos[1],
+                    pos[0] + size[0], pos[1] + size[1],
+                    pos2[0], pos2[1],
+                    pos2[0] + size2[0], pos2[1] + size2[1]);
+}
+
+function checkCollisions() {
+    //checkPlayerBounds();
+
+    var pos = [g1.client_pos.x, g1.client_pos.y];
+    var size = [g1.client_sprite_size.w, g1.client_sprite_size.h];
+
+	if( g1.client_live )
+	{
+		for(var j=0; j<bullets.length; j++) {
+			var pos2 = bullets[j].pos;
+			var size2 = bullets[j].spriteSize;
+
+			if(boxCollides(pos, size, pos2, size2)) {
+				g1.client_live = false;
+				g1.client_dead_time = Date.now();
+
+				// Add score
+				score += 100;
+
+				// broadcast
+                g1.player_host.emit('collide', g1.client_pos, score);
+                g1.player_client.emit('collide', g1.client_pos, score);
+
+                // broadcast
+                g1.player_host.emit('bullet remove', j);
+                g1.player_client.emit('bullet remove', j);
+				break;
+			}
+		}
+
+		if(boxCollides(pos, size, g1.host_pos, g1.host_sprite_size)) {
+			//gameOver();
+		}
+	}
+}
